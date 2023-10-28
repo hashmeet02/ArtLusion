b644a3bc3223f5dcb792647ba654aafe90faf23f
Hashmeet Singh Saini <103609995+hashmeet02@users.noreply.github.com>
2023-10-28 19:28:01 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,3 @@
-/*********************************************************************************
-*  WEB422 – Assignment 4
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: Hashmeet Singh Saini Student ID: 153070214 Date: February 27th, 23 
-*
-*
-********************************************************************************/ 
-
-
 import { Row, Col } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 
